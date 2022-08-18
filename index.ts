b696fb25f9e09b1c553713c074be5da1b9af8d3c
Abhay0294 <abhaypratap349@gmail.com>
2022-08-18 08:59:15 +0530

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,13 +1,2 @@
 // Import stylesheets
 import './style.css';
-import * as React from 'react';
-function Hello(){
-  return <div></div>
-}
-// Write TypeScript code!
-let myDiv: JSX.Element;
-let C1 = (): JSX.Element => <div>test</div>;
-let c1 = <C1 />;
-
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
